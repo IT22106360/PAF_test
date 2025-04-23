ce7eec674a9738d13f73bfdaba6392697acc2b77
savindu-c <it22230874@my.sliit.lk>
2025-04-23 23:35:36 +0530

    post features

---
+++ Back-end/learning_app/src/main/java/learning/app/learning_app/Entity/Post.java
@@ -0,0 +1,28 @@
+package learning.app.learning_app.Entity;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+
+@Data
+@Document("posts")
+public class Post {
+    @Id
+    private ObjectId id;
+
+    private String content;
+    private String mediaUrl;
+    private LocalDateTime createdAt;
+
+    private ObjectId userId;
+
+    private Set<ObjectId> likedBy = new HashSet<>();
+
+    private List<ObjectId> commentIds = new ArrayList<>();
+
+    // Getters and Setters
+}
