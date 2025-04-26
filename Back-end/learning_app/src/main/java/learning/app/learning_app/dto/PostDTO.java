64a8f4407747b5165a56f547f5498c76b3fc9928
it22283894 <128390727+it22283894@users.noreply.github.com>
2025-04-26 10:39:30 +0530

    edit PostDTO

--- Back-end/learning_app/src/main/java/learning/app/learning_app/dto/PostDTO.java
+++ Back-end/learning_app/src/main/java/learning/app/learning_app/dto/PostDTO.java
@@ -4,10 +4,9 @@ package learning.app.learning_app.dto;
 import learning.app.learning_app.Entity.Post;
 import lombok.Data;
 
-
-// @Data
-// public class PostDTO {
-//     private Long id;
-//     private String content;
-//     private String mediaUrl;
-// }
+@Data
+public class PostDTO {
+    private Long id;
+    private String content;
+    private String mediaUrl;
+}
